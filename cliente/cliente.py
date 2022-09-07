8dbe8c46c2e3d04c413cf1e1895156844e179c71
Rebelskvll <raulemoncadat@gmail.com>
2022-09-06 21:53:56 -0500

    Socket casi terminado.

--- cliente/cliente.py
+++ cliente/cliente.py
@@ -1,16 +1,19 @@
 import socket
 import json
 
+HOST = "127.0.0.1"  # The server's hostname or IP address
+PORT = 65432  # The port used by the server
+
 diccionario = {
-    "llave1":"Hola",
-    "llave2":"Adiós"
+    "cuenta":"",
+    "valor":""
 }
 
-HOST = "192.168.0.3"  # The server's hostname or IP address
-PORT = 2040  # The port used by the server
-
+diccionario["cuenta"] = input ("Digite el número de la cuenta: ")
+diccionario["valor"] = input ("Digite el valor: ")
+serializado = json.dumps(diccionario).encode('utf-8')
 with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
     s.connect((HOST, PORT))
-    b = json.dumps(diccionario).encode('utf-8')
-    s.sendall(b)
+    s.sendall(serializado)
     data = s.recv(1024)
+print(data.decode("utf-8"))
--- servidor/server.py
+++ servidor/server.py
@@ -1,7 +1,6 @@
 #!/usr/bin/python3
 
 """
-Ejemplos tomados de: 
 - https://realpython.com/python-sockets/
 - https://itecnote.com/tecnote/python-how-to-send-the-content-of-a-dictionary-properly-over-sockets-in-python3x/
 - https://docs.python.org/es/3/howto/sockets.html
@@ -11,33 +10,24 @@ Ejemplos tomados de:
 import socket
 import json
 
-# Puerto por el cual se van a aceptar conexiones
-host = "192.168.0.3"
-puerto = 2040
+HOST = "127.0.0.1"
+PORT = 65432
 
-
-# Se hace la creación del socket
-with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as socketServer:
-
-    socketServer.bind ((host, puerto))
-    socketServer.listen(5)
-
-    conexion = socketServer.accept()
-
-    b = b''
-
-
-    while 1:
-        tmp = conexion.recv(1024)
-        b += tmp
-
-    d = json.loads(b.decode('UTF-8'))
-
-    print (d)
-
-
-
-
-def guardarDatos(datos):
-    
+def crearArchivo():
     pass
+
+while True: 
+    with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+        s.bind((HOST, PORT))
+        s.listen()
+        conn, addr = s.accept()
+        with conn:
+            print(f"Cliente {addr[0]} conectado")
+            while 1:
+                datos = conn.recv(1024)
+                if not datos:
+                    break
+                deserializado = json.loads(datos.decode('utf-8'))
+                
+                conn.sendall(b"test")
+            
