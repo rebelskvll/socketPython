cdfa9473e1d88bcc7ed26f9e6d693b0bb3afd596
Rebelskvll <raulemoncadat@gmail.com>
2022-09-07 21:40:26 -0500

    Modificacion

--- cliente/cliente.py
+++ cliente/cliente.py
@@ -5,12 +5,12 @@ HOST = "127.0.0.1"  # The server's hostname or IP address
 PORT = 65432  # The port used by the server
 
 diccionario = {
-    "cuenta":"",
-    "valor":""
+    "cuenta":0,
+    "valor":0
 }
 
-diccionario["cuenta"] = input ("Digite el número de la cuenta: ")
-diccionario["valor"] = input ("Digite el valor: ")
+diccionario["cuenta"] = int(input ("Digite el número de la cuenta: "))
+diccionario["valor"] = int(input ("Digite el valor: "))
 serializado = json.dumps(diccionario).encode('utf-8')
 with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
     s.connect((HOST, PORT))
