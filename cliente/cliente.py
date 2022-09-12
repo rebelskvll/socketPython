e7810b501ba4009bebfdafe844113bc7925a4c92
Rebelskvll <raulemoncadat@gmail.com>
2022-09-11 22:29:53 -0500

    Cambios menores

--- cliente/cliente.py
+++ cliente/cliente.py
@@ -1,8 +1,8 @@
 import socket
 import json
 
-HOST = "127.0.0.1"  # The server's hostname or IP address
-PORT = 65432  # The port used by the server
+HOST = "127.0.0.1"
+PORT = 65432
 
 diccionario = {
     "cuenta":0,
